ac17ccf4f95472054cef11852068cd6b815cc433
GCgang <powm97@gmail.com>
2024-04-28 21:23:24 +0900

    [Feat]: 비교 연산자

---
+++ operator/6.relational.js
@@ -0,0 +1,15 @@
+// 대소 관계 비교 연산자 (Relational operators)
+
+// > 크자
+// < 작다
+// >= 크거나 같다
+// <= 작거나 크다
+
+console.log(100 > 1); // true
+console.log(1 < 100); // true
+
+console.log(100 >= 100); // true
+console.log(100 >= 101); // false
+
+console.log(1 <= 1); // true
+console.log(1 <= 0); // false
