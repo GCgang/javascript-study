27dcea4d9c7da3c81bc63ccf28cdbb5f86f14236
GCgang <powm97@gmail.com>
2024-05-09 11:30:38 +0900

    [Feat]: Boolean 객체

---
+++ built-in/3.boolean.js
@@ -0,0 +1,35 @@
+// Boolean
+const isTrue = new Boolean(true);
+console.log(typeof isTrue); // object
+console.log(isTrue); // [Boolean: true]
+console.log(isTrue.valueOf()); // true
+
+/**
+ * Falsy
+ * null
+ * undefined
+ * false
+ * NaN
+ * 0
+ * -0
+ * 0n
+ * ""
+ */
+
+
+/**
+ * Truthy
+ * true
+ * {}
+ * []
+ * new Date()
+ * 42
+ * -42
+ * 42n
+ * "0"
+ * "false"
+ * 3.14
+ * -3.14
+ * Infinity
+ * -Infinity
+ */
