9556e675ed739982f760d7983182746ab22fc119
GCgang <powm97@gmail.com>
2024-05-13 19:32:41 +0900

    [Feat]: module

---
+++ module/main.js
@@ -0,0 +1,10 @@
+// import { increase  } from './counter.js';
+// import { increase, getCount } from './counter.js';
+import * as counter from './counter.js';
+
+counter.increase();
+counter.increase();
+console.log(counter.count);
+console.log(counter.getCount());
+
+
