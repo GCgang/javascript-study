16e632fe08ce35e30ee5c455733a505961e05633
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-06-10 01:26:46 +0900

    [Feat]: 좌표 찾기 css 작업, event 처리

--- browser/coordinates/src/main.js
+++ browser/coordinates/src/main.js
@@ -1,3 +1,20 @@
+const horizontal = document.querySelector('.line-horizontal');
+const vertical = document.querySelector('.line-vertical');
+const target = document.querySelector('.target');
+const screenLog = document.getElementById('screen-log');
 
 document.addEventListener('mousemove', (event) => {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+
+    horizontal.style.top = `${mouseY}px`
+    vertical.style.left = `${mouseX}px`
+    target.style.top = `${mouseY}px`
+    target.style.left = `${mouseX}px`
+    screenLog.style.top = `${mouseY}px`
+    screenLog.style.left = `${mouseX}px`
+
+    screenLog.innerHTML = `
+        ${event.clientX}px ${event.clientY}px
+    `
 })
