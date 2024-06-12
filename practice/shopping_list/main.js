d42af4dbc2e12947f9aea779c2abd69e8bf468e5
GCgang <powm97@gmail.com>
2024-06-12 16:53:09 +0900

    [Feat]: createItem

--- practice/shopping_list/main.js
+++ practice/shopping_list/main.js
@@ -4,6 +4,16 @@ const addBtn = document.querySelector('.footer__add');
 
 items.addEventListener('click', onDelete);
 addBtn.addEventListener('click', onAdd);
+input.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        onAdd();
+    }
+});
+
+function onDelete(event) {
+    event.target.closest('.item__delete')?.closest('li')?.remove();
+}
+
 function onAdd(event) {
     const text = input.value;
     if (text.trim() === '') {
@@ -11,9 +21,37 @@ function onAdd(event) {
         return ;
     }
 
+    const itemRow = createItem(text);
 
     items.appendChild(itemRow);
 
     input.value = '';
     input.focus();
+}
+
+function createItem(text) {
+    const itemRow = document.createElement('li');
+    itemRow.setAttribute('class', 'item__row');
+
+    const item = document.createElement('div');
+    item.setAttribute('class', 'item');
+
+    const itemName = document.createElement('span');
+    itemName.setAttribute('class', 'item__name');
+    itemName.textContent = text;
+
+    const itemDelete = document.createElement('button');
+    itemDelete.setAttribute('class', 'item__delete');
+    itemDelete.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+
+    const itemDivider = document.createElement('div');
+    itemDivider.setAttribute('class', 'item__divider');
+
+    item.appendChild(itemName);
+    item.appendChild(itemDelete);
+
+    itemRow.appendChild(item);
+    itemRow.appendChild(itemDivider);
+
+    return itemRow;
 }
