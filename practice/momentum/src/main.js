a2f1d02a5b496f9582483705281e4334629293f3
GCgang <powm97@gmail.com>
2024-06-18 14:00:16 +0900

    Feat: login-form

---
+++ practice/momentum/src/main.js
@@ -0,0 +1,29 @@
+
+const loginForm = document.querySelector('.login-form');
+const loginInput = document.querySelector('.login-form__input')
+const greeting = document.querySelector('#greeting');
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+const USERNAME_KEY = 'username';
+const HIDDEN_CLASSNAME = 'hidden'
+
+if (savedUserName === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener('submit', onSubmit);
+} else {
+    paintGreetings(savedUserName);
+}
+
+function onSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
+    greeting.innerText = `Hi ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
